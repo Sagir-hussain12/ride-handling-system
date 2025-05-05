1d75f80251fbfbe86400c351a4620f153a8934d2
Sagir-hussain12 <sagiransary18@gmail>
2025-05-05 17:58:09 +0530

    final commit

--- Backend/models/blacklistToken.model.js
+++ Backend/models/blacklistToken.model.js
@@ -13,4 +13,5 @@ const blacklistTokenSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model('BlacklistToken', blacklistTokenSchema);
+const  blackListTokenModel = mongoose.model('BlackListToken', blacklistTokenSchema);
+module.exports = blackListTokenModel
